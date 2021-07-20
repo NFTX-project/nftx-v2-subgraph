28d28745bbd06e8e94e6be4c4d7995cb7cfa3254
Nick <nick@ethereumprice.org>
2021-07-20 14:56:06 +0100

    zap tests

--- src/mappings/zap.ts
+++ src/mappings/zap.ts
@@ -6,23 +6,27 @@ import {
 import { NFTXVaultFactoryUpgradeable as NFTXVaultFactory } from '../types/templates/NFTXLPStaking/NFTXVaultFactoryUpgradeable';
 
 import {
+  getStakedLpUser,
+  getVault,
   getZap
 } from './helpers';
 
 export function handleUserStaked(event: UserStakedEvent): void {
   let lockEndTime = event.params.timelockUntil;
   let vaultId = event.params.vaultId
+  let user = getStakedLpUser(event.transaction.from);
 
   let zapInstance = NFTXStakingZap.bind(event.address);
   let vaultFactoryAddress = zapInstance.nftxFactory();
   let vaultFactoryInstance = NFTXVaultFactory.bind(vaultFactoryAddress);
   let vaultAddress = vaultFactoryInstance.vault(vaultId);
+  let vault = getVault(vaultAddress);
 
   let txHash = event.transaction.hash;
   let zap = getZap(txHash);
 
-  zap.vault = vaultAddress.toHexString();
-  zap.user = event.params.sender.toHexString();
+  zap.vault = vault.id;
+  zap.user = user.id;
   zap.lockEndTime = lockEndTime
 
   zap.save();
