05d97309609d32b034bbaa5079a5a82457c687b6
Nick <nick@ethereumprice.org>
2021-07-20 13:27:19 +0100

    user address working

--- src/mappings/zap.ts
+++ src/mappings/zap.ts
@@ -11,17 +11,11 @@ import {
 
 export function handleUserStaked(event: UserStakedEvent): void {
   let lockEndTime = event.params.timelockUntil;
-  let vaultId = event.params.vaultId
-
-  let zapInstance = NFTXStakingZap.bind(event.address);
-  let vaultFactoryAddress = zapInstance.nftxFactory();
-  let vaultFactoryInstance = NFTXVaultFactory.bind(vaultFactoryAddress);
-  let vaultAddress = vaultFactoryInstance.vault(vaultId);
 
   let txHash = event.transaction.hash;
   let zap = getZap(txHash);
 
-  zap.vault = vaultAddress.toHexString();
+  zap.vault = "0xdea9196dcdd2173d6e369c2acc0facc83fd9346a";
   zap.user = event.params.sender.toHexString();
   zap.lockEndTime = lockEndTime
 
