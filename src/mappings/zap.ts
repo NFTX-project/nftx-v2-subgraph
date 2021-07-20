1da3a732be7a39fc2a16e93c28cf80bba38e6166
Nick <nick@ethereumprice.org>
2021-07-20 16:24:07 +0100

    abi added

--- src/mappings/zap.ts
+++ src/mappings/zap.ts
@@ -21,12 +21,14 @@ export function handleUserStaked(event: UserStakedEvent): void {
   let vaultFactoryAddress = zapInstance.nftxFactory();
   let vaultFactoryInstance = NFTXVaultFactory.bind(vaultFactoryAddress);
   let vaultAddress = vaultFactoryInstance.vault(vaultId);
+  let vault = getVault(vaultAddress);
+  vault.save();
 
   let txHash = event.transaction.hash;
   let zap = getZap(txHash);
 
-  zap.vault = vaultAddress.toHexString();
-  zap.user = event.params.sender.toHexString();
+  zap.vault = vault.id;
+  zap.user = user.id;
   zap.lockEndTime = lockEndTime
 
   zap.save();
