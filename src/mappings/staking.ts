ec1aab9942385c0236258d474f115b0defbf2f4e
Justin <justincavery@gmail.com>
2021-12-20 10:39:25 +0000

    Update staking.ts
    Removed all references to the new Inventory pool in the staking files.

--- src/mappings/staking.ts
+++ src/mappings/staking.ts
@@ -3,44 +3,44 @@ import {
   PoolUpdated as PoolUpdatedEvent,
   NFTXLPStaking,
 } from '../types/templates/NFTXLPStaking/NFTXLPStaking';
-import {
-  XTokenCreated as XTokenCreatedEvent,
-  NFTXInventoryStaking
-} from '../types/templates/NFTXInventoryStaking/NFTXInventoryStaking';
+// import {
+//   XTokenCreated as XTokenCreatedEvent,
+//   NFTXInventoryStaking
+// } from '../types/templates/NFTXInventoryStaking/NFTXInventoryStaking';
 import { NFTXVaultFactoryUpgradeable as NFTXVaultFactory } from '../types/templates/NFTXLPStaking/NFTXVaultFactoryUpgradeable';
 import { StakingTokenProvider } from '../types/templates/NFTXLPStaking/StakingTokenProvider';
-import { getInventoryPool, getPool, getToken, getVault } from './helpers';
+import { getPool, getToken, getVault } from './helpers';
 import { RewardDistributionTokenUpgradeable as RewardDistributionTokenTemplate } from '../types/templates';
 import { Address, BigInt } from '@graphprotocol/graph-ts';
 
-function newInventoryPool(
-  stakingAddress: Address,
-  xTokenAddress: Address,
-  vaultId: BigInt
-): void {
-  let stakingInstance = NFTXInventoryStaking.bind(stakingAddress);
-  let vaultFactoryAddress = stakingInstance.nftxVaultFactory();
-  let vaultFactoryInstance = NFTXVaultFactory.bind(vaultFactoryAddress);
-  let vaultAddress = vaultFactoryInstance.vault(vaultId);
+// function newInventoryPool(
+//   stakingAddress: Address,
+//   xTokenAddress: Address,
+//   vaultId: BigInt
+// ): void {
+//   let stakingInstance = NFTXInventoryStaking.bind(stakingAddress);
+//   let vaultFactoryAddress = stakingInstance.nftxVaultFactory();
+//   let vaultFactoryInstance = NFTXVaultFactory.bind(vaultFactoryAddress);
+//   let vaultAddress = vaultFactoryInstance.vault(vaultId);
 
-  let pool = getInventoryPool(xTokenAddress);
-  let vault = getVault(vaultAddress);
-  vault.inventoryStakingPool = pool.id;
-  vault.save();
+//   let pool = getInventoryPool(xTokenAddress);
+//   let vault = getVault(vaultAddress);
+//   vault.inventoryStakingPool = pool.id;
+//   vault.save();
 
-  let rewardToken = getToken(vaultAddress);
-  rewardToken.save();
-  let dividendToken = getToken(xTokenAddress);
-  dividendToken.save();
-  let stakingToken = getToken(vaultAddress);
-  stakingToken.save();
+//   let rewardToken = getToken(vaultAddress);
+//   rewardToken.save();
+//   let dividendToken = getToken(xTokenAddress);
+//   dividendToken.save();
+//   let stakingToken = getToken(vaultAddress);
+//   stakingToken.save();
 
-  pool.rewardToken = rewardToken.id;
-  pool.stakingToken = stakingToken.id;
-  pool.dividendToken = dividendToken.id;
-  pool.vault = vaultAddress.toHexString();
-  pool.save();
-}
+//   pool.rewardToken = rewardToken.id;
+//   pool.stakingToken = stakingToken.id;
+//   pool.dividendToken = dividendToken.id;
+//   pool.vault = vaultAddress.toHexString();
+//   pool.save();
+// }
 
 function newPool(
   stakingAddress: Address,
@@ -81,9 +81,9 @@ function newPool(
   RewardDistributionTokenTemplate.create(poolAddress);
 }
 
-export function handleXTokenCreated(event: XTokenCreatedEvent): void {
-  newInventoryPool(event.address, event.params.xToken, event.params.vaultId)
-}
+// export function handleXTokenCreated(event: XTokenCreatedEvent): void {
+//   newInventoryPool(event.address, event.params.xToken, event.params.vaultId)
+// }
 
 export function handlePoolCreated(event: PoolCreatedEvent): void {
   newPool(event.address, event.params.pool, event.params.vaultId, event.block.number);
