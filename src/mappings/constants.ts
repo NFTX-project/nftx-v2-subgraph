2117d0e4001cc70fbba4bf4819551b2356caae7d
Justin <justincavery@gmail.com>
2022-01-10 14:04:02 +0000

    change var to let

--- src/mappings/constants.ts
+++ src/mappings/constants.ts
@@ -1,5 +1,5 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts';
 
-export var ADDRESS_ZERO: Address = Address.fromHexString(
+export let ADDRESS_ZERO: Address = Address.fromHexString(
   '0x0000000000000000000000000000000000000000',
 ) as Address;
