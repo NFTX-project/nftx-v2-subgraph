3eab7c35d38999444411e7e274c5b2dd981127e9
Simon Emanuel Schmid <simon@edgeandnode.com>
2022-01-12 12:23:55 +0000

    Matchstick smoke test

---
+++ tests/general.test.ts
@@ -0,0 +1,6 @@
+import { clearStore, test, assert } from 'matchstick-as/assembly/index';
+import { log } from 'matchstick-as/assembly/log';
+
+test('Matchstick runs', () => {
+  log.success('Matchstick runs', []);
+});
